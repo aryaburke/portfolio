32c2b4eab96d30852f97f34b586ba9d46ffeb6f4
arya <aryaburke4@gmail.com>
2021-05-06 22:50:14 -0400

    poem annotated

--- src/writing/poem.js
+++ src/writing/poem.js
@@ -3,9 +3,40 @@ let text = "there is faith and decay in the mist:\na canticle. we return and ret
 
 let db = [
     {
-        start: 1,
-        end: 25,
-        annotation: "here i did this thing"
+        /*a canticle*/
+        start: 37,
+        end: 48,
+        annotation: "religious tone set early on in the poem"
+    },
+    {
+        /*rust upon these vines*/
+        start: 71,
+        end: 103,
+        annotation: "the intertwining of biological and mechanical"
+    },
+    {
+        /*the patterns on my skin*/
+        start: 131,
+        end:155,
+        annotation: "tying the dialectic into my own body"
+    },
+    {
+        /*now our days...*/
+        start: 344,
+        end: 411, 
+        annotation: "the act of forging, draining, repairing of the body"
+    },
+    {
+        /*tetsuo*/
+        start: 575,
+        end: 590,
+        annotation: "tetsuo is a reference to both the movie tetsuo: the iron man (1989) and akira (1988). in both movies, the character tetsuo deforms into a horrifying mess of flesh and wires. this is the central call of this poem - to take this image of horror and reshape it into something sacred, something hopeful, something one does to onself rather than has done to them."
+    },
+    {
+        /*a body forged...*/
+        start: 698,
+        end: 790, 
+        annotation: "self and sacred and joy and technology and body are all intertwined and built together"
     }
 ]
 
